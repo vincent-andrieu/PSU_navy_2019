30aa52e0a6a7d688e5ce397e84593107b6ec1692
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-02-06 14:17:39 +0100

    - Add macros in the navy.h

--- include/navy.h
+++ include/navy.h
@@ -13,5 +13,17 @@
 #define NAVY_H_
 
 #define EXIT_ERROR 84
+#define FILE_SIZE 31
+#define NBR_BOATS 4
+#define MAP_SIZE 8
+
+#define HIT "x"
+#define MISS "o"
+#define SEA "."
+#define SEA_VALUE 0
+
+int navy(int argc, char **argv);
+char **get_player_map(char *filepath);
+bool check_map_errors(char **map, char *buffer);
 
 #endif
