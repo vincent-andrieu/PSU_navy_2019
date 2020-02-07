c74cb5ae924e7be7d22d51f3ff04db754b9b3cb0
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-02-07 14:24:57 +0100

    - Edit game_edit.c function take x and y not char *input

--- src/game_edit.c
+++ src/game_edit.c
@@ -8,23 +8,22 @@
 #include "my.h"
 #include "navy.h"
 
-int edit_map(player_t *player, const char *input)
+int edit_map(player_t *player, const int x, const int y)
 {
-    if (my_strlen(input) != 2)
-        return EXIT_ERROR;
-    if (input[0] < 65 || input[0] > 90 || input[1] < 48 || input[1] > 57)
-        return EXIT_ERROR;
     my_putstr("attack: ");
-    my_putstr(input);
+    my_putchar(x + 'A');
+    my_putchar(y + '0');
     my_putchar('\n');
-    if (player->map_player[input[1] - 48 - 1][input[0] - 65] > 0) {
-        player->map_player[input[1] - 48 - 1][input[0] - 65] = HIT_VALUE;
-        my_putstr(input);
+    if (player->map_player[y - 1][x - 1] > 0) {
+        player->map_player[y - 1][x - 1] = HIT_VALUE;
+        my_putchar(x + 'A');
+        my_putchar(y + '0');
         my_putstr(": hit\n");
     }
     else {
-        player->map_player[input[1] - 48 - 1][input[0] - 65] = MISS_VALUE;
-        my_putstr(input);
+        player->map_player[y - 1][x - 1] = MISS_VALUE;
+        my_putchar(x + 'A');
+        my_putchar(y + '0');
         my_putstr(": missed\n");
     }
     return 0;
