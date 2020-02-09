61c3a591c9b467f0dacd600e1b33d43485dcae84
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-02-09 03:45:32 +0100

    - Add a function to get player maps - Fix function exit_navy to free all maps

--- src/navy.c
+++ src/navy.c
@@ -8,22 +8,40 @@
 #include "my.h"
 #include "navy.h"
 
-static int exit_navy(int **map, int exit_value)
+static int exit_navy(player_t *player, int exit_value)
 {
-    for (int i = 0; map[i] != NULL; free(map[i]), i++);
-    free(map);
+    for (int i = 0; player->map_player[i] != NULL; i++)
+        free(player->map_player[i]);
+    free(player->map_player);
+    for (int i = 0; player->map_enemy[i] != NULL; i++)
+        free(player->map_enemy[i]);
+    free(player->map_enemy);
+    free(player);
     return exit_value;
+}
+
+static player_t *get_player_maps(int argc, char **argv)
+{
+    player_t *player = malloc(sizeof(player_t));
+
+    if (player == NULL)
+        return NULL;
+    player->map_player = get_map(argv[argc == 3 ? 2 : 1]);
+    player->map_enemy = get_int_array();
+    if (player->map_player == NULL || player->map_enemy == NULL)
+        return NULL;
+    return player;
 }
 
 int navy(int argc, char **argv)
 {
-    int **map = get_map(argv[argc == 3 ? 2 : 1]);
+    player_t *player = get_player_maps(argc, argv);
     int enemy_pid;
 
-    if (map == NULL)
+    if (player == NULL)
         return EXIT_ERROR;
     enemy_pid = init_connection(argc, argv[1]);
     if (enemy_pid <= 0)
-        return exit_navy(map, EXIT_ERROR);
-    return exit_navy(map, EXIT_SUCCESS);
+        return exit_navy(player, EXIT_ERROR);
+    return exit_navy(player, game(player, enemy_pid, argc == 2));
 }
