ed184d365365396514a385531ef50e7330eb20bf
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-02-10 11:40:50 +0100

    - Fix check some free

--- src/navy.c
+++ src/navy.c
@@ -10,10 +10,12 @@
 
 static int exit_navy(player_t *player, int exit_value)
 {
-    for (int i = 0; player->map_player[i] != NULL; i++)
+    if (player == NULL)
+        return exit_value;
+    for (int i = 0; player->map_player && player->map_player[i] != NULL; i++)
         free(player->map_player[i]);
     free(player->map_player);
-    for (int i = 0; player->map_enemy[i] != NULL; i++)
+    for (int i = 0; player->map_enemy && player->map_enemy[i] != NULL; i++)
         free(player->map_enemy[i]);
     free(player->map_enemy);
     free(player);
@@ -29,8 +31,10 @@ static player_t *get_player_maps(int argc, char **argv)
     player->toggle = argc == 2;
     player->map_player = get_map(argv[argc == 3 ? 2 : 1]);
     player->map_enemy = get_int_array();
-    if (player->map_player == NULL || player->map_enemy == NULL)
+    if (player->map_player == NULL || player->map_enemy == NULL) {
+        exit_navy(player, EXIT_ERROR);
         return NULL;
+    }
     return player;
 }
 
@@ -40,7 +44,7 @@ int navy(int argc, char **argv)
     int enemy_pid;
 
     if (player == NULL)
-        return EXIT_ERROR;
+        return exit_navy(player, EXIT_ERROR);
     enemy_pid = init_connection(argc, argv[1]);
     if (enemy_pid <= 0)
         return exit_navy(player, EXIT_ERROR);
