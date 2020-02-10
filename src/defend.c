872e519a48760bd00477febe48177e208eee61f2
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-02-10 10:50:25 +0100

    - Remove game_edit.c file to move function into defend.c

--- src/defend.c
+++ src/defend.c
@@ -9,6 +9,21 @@
 #include "my.h"
 #include "navy.h"
 
+static void edit_map(player_t *player, int pid, int const x, int const y)
+{
+    my_putchar(x + 'A' - 1);
+    my_putchar(y + '0');
+    if (player->map_player[y - 1][x - 1] > 0) {
+        player->map_player[y - 1][x - 1] = HIT_VALUE;
+        my_putstr(": hit\n\n");
+        send_values(pid, 1, 0);
+    } else {
+        player->map_player[y - 1][x - 1] = MISS_VALUE;
+        my_putstr(": missed\n\n");
+        send_values(pid, 0, 1);
+    }
+}
+
 int defend(player_t *player, int pid)
 {
     receive_t receive;
